01b5b32d52c6bbd24c35d176eee0ccda1b408e69
Rehan <rehanalire52@gmail.com>
2024-11-28 21:40:40 +0530

    feat: integrate Zod validation for prompt input and improve error handling

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -1,3 +1,4 @@
+import z from "zod";
 import axios from "axios";
 import { uploadOnCloudinary } from "@/utils/cloudinary";
 import { NextRequest, NextResponse } from "next/server";
@@ -5,9 +6,15 @@ import { NextRequest, NextResponse } from "next/server";
 const HUGGINGFACE_API_KEY = process.env.HUGGINGFACE_API_KEY as string;
 const HUGGINGFACE_API_URL = process.env.HUGGINGFACE_API_URL as string;
 
+const promptValidation = z.object({
+  prompt: z.string().max(100, "Prompt must be less than 100 characters"),
+});
+
 export const POST = async (req: NextRequest) => {
   try {
-    const { prompt } = await req.json();
+    const body = await req.json();
+
+    const prompt = promptValidation.parse(body);
 
     const response = await axios.post(HUGGINGFACE_API_URL, prompt, {
       headers: {
@@ -20,7 +27,6 @@ export const POST = async (req: NextRequest) => {
     const imageBuffer = Buffer.from(response.data);
 
     const cloudinaryResponse = await uploadOnCloudinary(imageBuffer);
-    console.log(cloudinaryResponse);
 
     if (!cloudinaryResponse) {
       return NextResponse.json(
@@ -33,7 +39,13 @@ export const POST = async (req: NextRequest) => {
 
     return NextResponse.json({ imageUrl });
   } catch (error) {
-    console.error("Error:", error);
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        { success: false, message: error.errors[0]?.message || "Invalid input" },
+        { status: 400 }
+      );
+    }
+    console.error("Unexpected error:", error);
     return NextResponse.json(
       { success: false, message: "Error generating image" },
       { status: 500 }
