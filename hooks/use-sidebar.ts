63e6b74ae796d15eb96b333825734e5fb042a36a
Rehan <rehanalire52@gmail.com>
2024-12-02 23:35:01 +0530

    feat: Add zustand hook for sidebar

---
+++ hooks/use-sidebar.ts
@@ -0,0 +1,14 @@
+"use client"
+
+import { create } from "zustand"
+
+interface SidebarState {
+  isOpen: boolean
+  toggle: () => void
+}
+
+export const useSidebarState = create<SidebarState>((set) => ({
+  isOpen: true,
+  toggle: () => set((state) => ({ isOpen: !state.isOpen })),
+}))
+
