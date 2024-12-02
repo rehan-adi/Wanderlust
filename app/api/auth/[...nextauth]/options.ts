bfc018657b396c677ae956e813d658316712be9f
Rehan <rehanalire52@gmail.com>
2024-12-02 17:34:05 +0530

    Add NextAuthOptions to authOptions

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,7 +1,8 @@
 import prisma from "@/lib/prisma";
+import { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
