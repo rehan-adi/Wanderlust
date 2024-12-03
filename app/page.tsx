82478f2cc8d609fa33cedcad11f043a3794b7435
Rehan <rehanalire52@gmail.com>
2024-12-03 22:13:33 +0530

    Add input box and button

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,10 @@
 "use client";
 
-import { useSession } from "next-auth/react";
+import { Send } from "lucide-react";
 import { useSidebarState } from "@/hooks/use-sidebar";
 
 export default function Home() {
-  const { data: session, status } = useSession();
   const { isOpen } = useSidebarState();
-
-  if (status === "loading") {
-    return (
-      <div className="bg-[#060423] text-white flex justify-center items-center min-h-screen w-full">
-        Loading...
-      </div>
-    );
-  }
-
-  if (!session) {
-    return (
-      <div
-        className={`bg-[#060423] text-white min-h-screen flex justify-center items-center transition-all duration-300 ${
-          isOpen ? "md:ml-[13vw]" : "ml-0"
-        }`}
-      >
-        No session found
-      </div>
-    );
-  }
 
   return (
     <div
@@ -33,16 +12,17 @@ export default function Home() {
         isOpen ? "md:ml-[13vw]" : "ml-0"
       }`}
     >
-      <div className="flex flex-col items-center">
-        <img
-          src={session.user?.image || "/default-avatar.png"}
-          alt={session.user?.name || "User"}
-          className="rounded-full w-32 h-32 mb-4"
-        />
-        <h1 className="text-2xl">{session.user?.name || "No Name"}</h1>
-        <h1 className="text-2xl">{session.user?.id || "No ID"}</h1>{" "}
-        {/* Display userId */}
-        <p className="text-lg">{session.user?.email || "No Email"}</p>
+      <div className="flex flex-col fixed md:bottom-20 bottom-10 items-center">
+        <div className="flex gap-3 items-center">
+          <input
+            type="text"
+            placeholder="Describe your image"
+            className="py-3 px-7 bg-[#1b1b2f] rounded-2xl border border-white border-opacity-20 md:w-[40vw] w-[75vw] text-white placeholder-opacity-50 focus:outline-none"
+          />
+          <button className="flex items-center gap-2 py-3 px-5 bg-white text-black rounded-2xl transition-transform border border-white border-opacity-20">
+            <Send size={18} />
+          </button>
+        </div>
       </div>
     </div>
   );
