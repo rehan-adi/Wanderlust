45a4ace829bbfa9c5ffda9e2435b32542ade5235
Rehan <rehanalire52@gmail.com>
2024-12-07 21:18:17 +0530

    Add logo svg and add Generate button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,11 +21,13 @@ export default function Navbar() {
   return (
     <>
       <nav className="fixed top-0 z-20 h-[60px] bg-white border border-black border-opacity-15 w-full text-black">
-        <div className="flex h-full items-center justify-between md:px-12 px-4">
+        <div className="flex h-full items-center justify-between md:px-20 px-4">
           <div>
             <Link href="/">
               <span className="font-bold md:text-xl text-base text-black">
-                Wanderlust
+                <svg width="30" height="16" viewBox="0 0 30 16">
+                  <path d="m18.4 0-2.803 10.855L12.951 0H9.34L6.693 10.855 3.892 0H0l5.012 15.812h3.425l2.708-10.228 2.709 10.228h3.425L22.29 0h-3.892ZM24.77 13.365c0 1.506 1.12 2.635 2.615 2.635C28.879 16 30 14.87 30 13.365c0-1.506-1.12-2.636-2.615-2.636s-2.615 1.13-2.615 2.636Z"></path>
+                </svg>
               </span>
             </Link>
           </div>
@@ -36,10 +38,15 @@ export default function Navbar() {
               </span>
             ) : session?.user?.id ? (
               <>
+                <Link href="/generate">
+                  <button className="bg-black text-white flex text-sm font-semibold justify-center items-center rounded-lg py-2 px-4">
+                    Generate
+                  </button>
+                </Link>
                 <img
                   src={session.user.image || "/default-avatar.png"}
                   alt="User Avatar"
-                  className="w-8 h-8 rounded-full cursor-pointer"
+                  className="w-9 h-9 rounded-full cursor-pointer"
                   onClick={toggleProfileMenu}
                 />
                 {profileMenu && (
@@ -50,7 +57,7 @@ export default function Navbar() {
                     <ul className="p-2">
                       <li className="px-4 py-2 rounded-xl hover:bg-gray-100 flex cursor-pointer">
                         <Link href="/profile">
-                          <User size={17} className="inline-block mr-2"/>
+                          <User size={17} className="inline-block mr-2" />
                           Profile
                         </Link>
                       </li>
@@ -58,7 +65,7 @@ export default function Navbar() {
                         className="px-4 py-2 rounded-xl hover:bg-gray-100 cursor-pointer text-red-500"
                         onClick={() => signOut()}
                       >
-                         <LogIn size={17} className="inline-block mr-2"/>
+                        <LogIn size={17} className="inline-block mr-2" />
                         Log out
                       </li>
                     </ul>
