7555d51f2ed5153a8ca8f6c9162d33551907258a
Rehan <rehanalire52@gmail.com>
2024-12-08 14:21:18 +0530

    Update navbar to include settings link and improve session handling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,15 +2,18 @@
 
 import Link from "next/link";
 import Signin from "./Signin";
-import { useState } from "react";
-import { Loader2, User, LogIn } from "lucide-react";
+import Image from "next/image";
+import { User, LogIn, Settings, Loader2 } from "lucide-react";
+import { useState, useRef, useEffect } from "react";
 import { useSession, signOut } from "next-auth/react";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Navbar() {
   const { data: session, status } = useSession();
 
   const [modal, setModal] = useState(false);
   const [profileMenu, setProfileMenu] = useState(false);
+  const profileMenuRef = useRef<HTMLDivElement>(null);
 
   const openSignin = () => setModal(true);
   const closeSignin = () => setModal(false);
@@ -18,64 +21,99 @@ export default function Navbar() {
   const toggleProfileMenu = () => setProfileMenu(!profileMenu);
   const closeProfileMenu = () => setProfileMenu(false);
 
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      profileMenuRef.current &&
+      !profileMenuRef.current.contains(event.target as Node)
+    ) {
+      closeProfileMenu();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
   return (
     <>
-      <nav className="fixed top-0 z-20 h-[60px] bg-white border border-black border-opacity-15 w-full text-black">
+      <nav className="fixed top-0 z-20 h-[60px] bg-white border border-black border-opacity-10 w-full text-black">
         <div className="flex h-full items-center justify-between md:px-20 px-4">
           <div>
-            <Link href="/">
-              <span className="font-bold md:text-xl text-base text-black">
-                <svg width="30" height="16" viewBox="0 0 30 16">
-                  <path d="m18.4 0-2.803 10.855L12.951 0H9.34L6.693 10.855 3.892 0H0l5.012 15.812h3.425l2.708-10.228 2.709 10.228h3.425L22.29 0h-3.892ZM24.77 13.365c0 1.506 1.12 2.635 2.615 2.635C28.879 16 30 14.87 30 13.365c0-1.506-1.12-2.636-2.615-2.636s-2.615 1.13-2.615 2.636Z"></path>
-                </svg>
-              </span>
+            <Link href="/" className="flex-shrink-0">
+              <Image
+                src="/logo.svg"
+                alt="Logo"
+                width={28}
+                height={28}
+                priority
+                blurDataURL="data:image/svg+xml;base64,..."
+                placeholder="blur"
+              />
             </Link>
           </div>
           <div className="flex items-center space-x-4 relative">
             {status === "loading" ? (
-              <span>
-                <Loader2 className="w-5 h-5 animate-spin" />
-              </span>
+              <div className="flex items-center space-x-4">
+                <div className="w-[85px] h-[36px] bg-gray-300 animate-pulse rounded-lg" />
+                <div className="w-[36px] h-[36px] bg-gray-300 animate-pulse rounded-full" />
+              </div>
             ) : session?.user?.id ? (
               <>
                 <Link href="/generate">
-                  <button className="bg-black text-white flex text-sm font-semibold justify-center items-center rounded-lg py-2 px-4">
+                  <button className="bg-black text-white flex text-sm font-semibold justify-center items-center rounded-lg py-2 px-3">
                     Generate
                   </button>
                 </Link>
-                <img
-                  src={session.user.image || "/default-avatar.png"}
+                <Image
+                  src={session.user.image || "/default.png"}
                   alt="User Avatar"
-                  className="w-9 h-9 rounded-full cursor-pointer"
+                  width={36}
+                  height={36}
+                  blurDataURL="data:..."
+                  placeholder="blur"
+                  className="rounded-full cursor-pointer"
                   onClick={toggleProfileMenu}
+                  onError={(e) => (e.currentTarget.src = "/default.png")}
                 />
                 {profileMenu && (
-                  <div
-                    className="absolute top-2 right-0 mt-12 w-48 bg-white border border-black border-opacity-10 rounded-xl shadow-lg"
-                    onBlur={closeProfileMenu}
-                  >
-                    <ul className="p-2">
-                      <li className="px-4 py-2 rounded-xl hover:bg-gray-100 flex cursor-pointer">
-                        <Link href="/profile">
-                          <User size={17} className="inline-block mr-2" />
-                          Profile
-                        </Link>
-                      </li>
-                      <li
-                        className="px-4 py-2 rounded-xl hover:bg-gray-100 cursor-pointer text-red-500"
-                        onClick={() => signOut()}
-                      >
-                        <LogIn size={17} className="inline-block mr-2" />
-                        Log out
-                      </li>
-                    </ul>
-                  </div>
+                  <AnimatePresence>
+                    <motion.div
+                      ref={profileMenuRef}
+                      initial={{ opacity: 0, scale: 0.95 }}
+                      animate={{ opacity: 1, scale: 1 }}
+                      exit={{ opacity: 0, scale: 0.95 }}
+                      className="absolute top-2 right-0 mt-12 w-48 bg-white border border-black border-opacity-10 rounded-xl shadow-2xl"
+                    >
+                      <ul className="p-2">
+                        <li className="px-4 py-2 rounded-xl hover:bg-gray-100 font-medium flex cursor-pointer">
+                          <Link href="/profile">
+                            <User size={17} className="inline-block mr-2" />
+                            Profile
+                          </Link>
+                        </li>
+                        <li className="px-4 py-2 rounded-xl hover:bg-gray-100 font-medium flex cursor-pointer">
+                          <Link href="/settings">
+                            <Settings size={17} className="inline-block mr-2" />
+                            Settings
+                          </Link>
+                        </li>
+                        <li
+                          className="px-3 py-2 rounded-xl hover:bg-gray-100 cursor-pointer font-medium text-red-500"
+                          onClick={() => signOut()}
+                        >
+                          <LogIn size={17} className="inline-block mr-2" />
+                          Sign out
+                        </li>
+                      </ul>
+                    </motion.div>
+                  </AnimatePresence>
                 )}
               </>
             ) : (
               <button
                 onClick={openSignin}
-                className="bg-black text-white flex gap-3 text-sm font-semibold justify-center items-center rounded-lg py-2 px-5"
+                className="bg-black text-white flex gap-3 text-sm font-semibold justify-center items-center rounded-lg py-2 px-4"
               >
                 Sign In
               </button>
