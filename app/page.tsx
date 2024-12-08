01731bedb5dc69a28d1ff2f0c0393bf1a8e1c9eb
Rehan <rehanalire52@gmail.com>
2024-12-08 15:44:01 +0530

    Update UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,4 @@
-import Image from "next/image";
-import { Search } from "lucide-react";
-
-interface ImagesData {
-  imageUrl: string;
-  prompt: string;
-  user: [];
-  createdAt: Date;
-}
+import { Images } from "@/components/Image";
 
 async function getImages() {
   try {
@@ -19,53 +11,12 @@ async function getImages() {
   }
 }
 
-export default async function Explore() {
+export default async function ExplorePage() {
   const images = await getImages();
 
   return (
     <div className="bg-white text-black min-h-screen pt-24 pb-10 flex flex-col items-center justify-start px-4">
-      {/* Search bar */}
-
-      <div className="w-full max-w-2xl mb-10">
-        <div className="relative">
-          <input
-            type="text"
-            placeholder="Search for images..."
-            className="w-full px-4 py-3 pl-12 bg-white rounded-full text-gray-800 placeholder-gray-400 focus:outline-none border shadow-lg"
-          />
-          <Search
-            className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-500"
-            size={20}
-          />
-        </div>
-      </div>
-
-      {/* Image grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 w-full max-w-6xl">
-        {images.length > 0 ? (
-          images.map((img: ImagesData, Index: number) => (
-            <div
-              key={Index}
-              className="relative group rounded-xl overflow-hidden shadow-lg hover:scale-105 transform transition-all duration-300"
-            >
-              <Image
-                src={img.imageUrl}
-                alt={img.prompt || "Generated Image"}
-                width={500}
-                height={500}
-                blurDataURL="data:..."
-                placeholder="blur"
-                className="object-cover w-full h-64 md:h-80 rounded-xl group-hover:opacity-80 transition-opacity duration-300"
-              />
-              <div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black to-transparent opacity-80 text-white">
-                <p className="font-semibold truncate">{img.prompt}</p>
-              </div>
-            </div>
-          ))
-        ) : (
-          <p className="text-center text-lg text-gray-700">No images found.</p>
-        )}
-      </div>
+      <Images images={images} />
     </div>
   );
 }
---
+++ components/Image.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+interface ImagesData {
+  imageUrl: string;
+  prompt: string;
+  user: [];
+  createdAt: Date;
+}
+
+interface ImageGridProps {
+  images: ImagesData[];
+}
+
+export function Images({ images }: ImageGridProps) {
+  const [selectedImage, setSelectedImage] = useState<ImagesData | null>(null);
+
+  const openModal = (img: ImagesData) => {
+    setSelectedImage(img);
+  };
+
+  const closeModal = () => {
+    setSelectedImage(null);
+  };
+
+  return (
+    <div className="w-full max-w-6xl flex justify-center items-center">
+      <div className="w-full grid grid-cols-2 lg:grid-cols-4 gap-1.5">
+        {images.length > 0 ? (
+          images.map((img, index) => (
+            <div
+              key={index}
+              className="relative group rounded-xl bg-white overflow-hidden shadow-lg cursor-pointer"
+              onClick={() => openModal(img)}
+            >
+              <Image
+                src={img.imageUrl}
+                alt={img.prompt || "Generated Image"}
+                width={444}
+                height={444}
+                blurDataURL="data:..."
+                placeholder="blur"
+                className="object-cover w-full h-56 md:h-72 rounded-xl group-hover:opacity-50 transition-opacity duration-300"
+              />
+              {/* Hover prompt */}
+              <div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <p className="font-semibold truncate text-white">
+                  {img.prompt}
+                </p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p className="text-center text-lg text-gray-700">No images found.</p>
+        )}
+      </div>
+
+      {/* Modal */}
+      {selectedImage && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-80 px-2 flex justify-center items-center z-50"
+          onClick={closeModal}
+        >
+          <div
+            className="bg-white rounded-2xl w-full max-w-md overflow-hidden"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className="relative">
+              <Image
+                src={selectedImage.imageUrl}
+                alt={selectedImage.prompt || "Generated Image"}
+                width={600}
+                height={600}
+                className="object-cover w-full h-96"
+              />
+            </div>
+            <div className="px-4 text-center py-7">
+              <p className="text-base text-center font-medium">
+                {selectedImage.prompt}
+              </p>
+              <button
+                onClick={closeModal}
+                className="mt-10 mb-4 md:px-20 px-14 py-2 bg-[#ECECF1] text-black font-semibold rounded-md"
+              >
+                Downloade this Image
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
