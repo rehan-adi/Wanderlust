11cf6f9789ec28113feddcf4039ebeec22dc8439
Rehan <rehanalire52@gmail.com>
2024-12-25 19:14:04 +0530

    Revamp Footer component with logo link and updated copyright notice

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import { BrainCircuit } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='w-full p-4 bg-white text-black flex justify-center items-center'>
-        <p className='text-sm font-semibold'>@2024 Wanderlust. All rights reserved.</p>
+    <div className="w-full py-4 md:px-12 lg:px-40 px-4 bg-white text-black flex flex-col md:flex-row justify-between items-center">
+      <Link
+        href="/"
+        className="flex justify-center font-secondary text-xl items-center gap-3"
+      >
+        <BrainCircuit size={25} />
+        <span className="mt-1 mb-3 md:mb-0">Wanderlust AI</span>
+      </Link>
+      <p className="text-sm font-semibold">
+        &copy; {new Date().getFullYear()} Wanderlust. All rights reserved.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
