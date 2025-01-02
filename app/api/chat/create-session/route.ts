d4f6738f3a8b298cbe2d9d6812c0d2261779a2a3
Rehan <rehanalire52@gmail.com>
2025-01-02 15:03:06 +0530

    feat: Implement chat session creation endpoint with authorization check

---
+++ app/api/chat/create-session/route.ts
@@ -0,0 +1,30 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+
+export async function POST(req: Request) {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return new Response(JSON.stringify({ error: "Unauthorized" }), {
+        status: 401,
+      });
+    }
+
+    const userId = session.user.id;
+
+    const newSession = await prisma.chatSession.create({
+      data: { userId },
+    });
+
+    return NextResponse.json({ sessionId: newSession.id }, { status: 201 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+}
