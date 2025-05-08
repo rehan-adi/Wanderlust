160375847b7f359d420f094eff598f95fed84ebf
Rehan <rehanalire52@gmail.com>
2025-05-08 17:38:03 +0530

    feat: Refactor Pinecone API key handling to use centralized configuration

--- config/config.ts
+++ config/config.ts
@@ -2,3 +2,5 @@ export const GEMINI_API_KEY = process.env.GEMINI_API_KEY as string;
 
 export const HUGGINGFACE_MODEL = process.env.HUGGINGFACE_MODEL as string;
 export const HUGGINGFACE_API_KEY = process.env.HUGGINGFACE_API_KEY as string;
+
+export const PINECONE_API_KEY = process.env.PINECONE_API_KEY as string;
--- lib/pinecone.ts
+++ lib/pinecone.ts
@@ -1,6 +1,5 @@
+import { PINECONE_API_KEY } from "@/config/config";
 import { Pinecone } from "@pinecone-database/pinecone";
-
-const PINECONE_API_KEY = process.env.PINECONE_API_KEY as string;
 
 export const pinecone = new Pinecone({
   apiKey: PINECONE_API_KEY,
