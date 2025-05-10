ccca2b8537dcb60f7fef1773bc1e1032f37a707d
Rehan <rehanalire52@gmail.com>
2025-05-10 23:18:43 +0530

    fix: Enhance token retrieval in middleware for improved session handling

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,9 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("next-auth.session-token")?.value;
+  const token =
+    request.cookies.get("next-auth.session-token")?.value ||
+    request.cookies.get("__Secure-next-auth.session-token")?.value;
 
   const protectedRoutes = [
     "/dashboard",
